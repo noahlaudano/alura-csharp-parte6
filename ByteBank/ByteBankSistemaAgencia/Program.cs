4801543f6ef06947f94b5d6c2ff5b228e5cf043a
Nathalia Laudano <nathalialaudano@crazytechlabs.com.br>
2021-06-22 17:02:11 -0300

    aula 5 - classe object

--- ByteBank/ByteBankSistemaAgencia/Program.cs
+++ ByteBank/ByteBankSistemaAgencia/Program.cs
@@ -7,9 +7,20 @@ namespace ByteBank.ByteBankSistemaAgencia
     {
         static void Main(string[] args)
         {
-            ContaCorrente conta = new ContaCorrente(123, 456789);
-            Console.WriteLine(conta.Conta);
-            Console.ReadLine();
+            Cliente carlos_1 = new Cliente();
+            carlos_1.Nome = "Carlos";
+            carlos_1.CPF = "458.623.120-03";
+            carlos_1.Profissao = "Designer";
+
+            Cliente carlos_2 = new Cliente();
+            carlos_2.Nome = "Carlos";
+            carlos_2.CPF = "458.623.120-03";
+            carlos_2.Profissao = "Designer";
+
+            if (carlos_1.Equals(carlos_2))
+            {
+                Console.WriteLine("São iguais!");
+            }
         }
     }
 }
